35a414144cde64494bd020200acf7005ccee3292
Jon Schneider <jkschneider@gmail.com>
2015-08-28 14:36:10 -0700

    membership is eureka enabled, change port of eureka server

--- membership/src/main/groovy/com/netflix/membership/Membership.groovy
+++ membership/src/main/groovy/com/netflix/membership/Membership.groovy
@@ -3,6 +3,7 @@ package com.netflix.membership
 import groovy.transform.Canonical
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@EnableEurekaClient
 class Membership {
     static void main(String[] args) {
         new SpringApplicationBuilder(Membership).web(true).run(args)
